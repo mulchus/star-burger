1cc9e91b8c8c2b59233ab3a5526bcd3e695f9a72
mulchus <mulch@list.ru>
2023-08-09 22:59:02 +0300

    Added dj_database_url and move to postgres DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -1,5 +1,4 @@
 import os
-
 import dj_database_url
 
 from environs import Env
@@ -93,8 +92,9 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
